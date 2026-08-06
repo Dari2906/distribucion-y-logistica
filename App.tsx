b13d8f8aefd875afe3a476cdca5eb7816b420b38
Dari2906 <dtonanez1@gmail.com>
2026-08-05 23:16:17 -0300

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{padding: '20px', fontFamily: 'sans-serif'}}>
+      <h1>Distribucion y Logistica</h1>
+      <p>Ya deployó bien en Vercel 🚀</p>
+    </div>
+  )
+}
+
+export default App
